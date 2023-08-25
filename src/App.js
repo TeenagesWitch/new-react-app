a5768a7819f4c5592b4df4408c8deb4aebcbbbd0
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-25 15:39:34 +0700

    fix overflow

--- src/App.js
+++ src/App.js
@@ -48,10 +48,26 @@ function App() {
     <ChakraProvider>
     <body style={{backgroundColor: "#eebc17", height: "100vh"}}>
     <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
-    <Card border="1px solid white" borderRadius="5px" padding="20px">
-      <Box  >
+    <Card 
+    border="1px solid white" 
+    borderRadius="5px" 
+    padding="20px" 
+    maxHeight="90vh"
+    overflow="auto"
+    display="flex"
+    flexDirection="column"
+    css={{
+        '&::-webkit-scrollbar': {
+            width: '0px',
+            background: 'transparent'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            background: 'transparent'
+        }
+    }}>
+      <Box>
           <h1 style={{fontSize: "xx-large", textAlign: "center", fontWeight: "bold"}}>Chores ToDo List</h1>
-          <List spacing={2} maxWidth="500px" width="100%">
+          <List spacing={2} maxWidth="90vh" width="100%">
             {todos.map((todo, index) => (
               <ListItem key={index} display="flex" alignItems="center">
               <Flex width="100%" alignItems="center">
@@ -84,6 +100,8 @@ function App() {
           </List>
           <h1 style={{textAlign: "center", fontWeight:"bold"}}>Done: {countChecked()}</h1>
           <Input 
+            maxWidth="100%"
+            overflow="hidden"
             value={inputValue} 
             onChange={(e) => setInputValue(e.target.value)} 
             placeholder="Add todo" 
