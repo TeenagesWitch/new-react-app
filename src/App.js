4e1eec19faf4c4a974368373c6952334af5f75ce
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-28 11:00:18 +0700

    refactoring todo list, add homepage and counter

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import {
   Checkbox,
   useColorModeValue,
   Card,
-  Flex
+  Flex,
+  Select
 } from "@chakra-ui/react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link
+  Link,
+  useNavigate
 } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { DeleteIcon } from "@chakra-ui/icons";
@@ -52,53 +54,73 @@ function TodoList() {
   }
 
   return (
-    <div className="todo-list-page">
-      <div className="container">
-        <div className="flex-column">
-          <h1>Chores ToDo List</h1>
+    <ChakraProvider>
+    <body style={{backgroundColor: "#eebc17", height: "100vh"}}>
+    <Container display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
+    <Card 
+    border="1px solid white" 
+    borderRadius="20px" 
+    padding="20px" 
+    maxHeight="90vh"
+    overflow="auto"
+    display="flex"
+    flexDirection="column"
+    css={{
+        '&::-webkit-scrollbar': {
+            width: '0px',
+            background: 'transparent'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            background: 'transparent'
+        }
+    }}>
+      <Box>
+          <h1 style={{fontSize: "xx-large", textAlign: "center", fontWeight: "bold"}}>Chores ToDo List</h1>
           <List spacing={2} maxWidth="90vh" width="100%">
             {todos.map((todo, index) => (
               <ListItem key={index} display="flex" alignItems="center">
               <Flex width="100%" alignItems="center">
-                  <Checkbox
-                      isChecked={todo.checked}
-                      onChange={() => toggleCheck(index)}
-                      colorScheme="green"
-                      mr={2}
+                  <Checkbox 
+                      isChecked={todo.checked} 
+                      onChange={() => toggleCheck(index)} 
+                      colorScheme="green" 
+                      mr={2} 
                       borderColor={checkboxColor}
                   />
-                  <Box
+                  <Box 
                       minWidth="0"
                       width="calc(100% - 50px)"
-                      overflowWrap="break-word"
+                      overflowWrap="break-word" 
                       wordWrap="break-word">
                       {todo.text}
                   </Box>
-                  <IconButton
-                      icon={<DeleteIcon />}
-                      onClick={() => removeTodo()}
-                      colorScheme="red"
-                      variant="outline"
+                  <IconButton 
+                      icon={<DeleteIcon />} 
+                      onClick={() => removeTodo(index)} 
+                      colorScheme="red" 
+                      variant="outline" 
                       borderColor={trashIconColor}
                       color={trashIconColor}
                   />
               </Flex>
-          </ListItem>
+          </ListItem>          
             ))}
           </List>
-          <h1>Done: {countChecked()}</h1>
-          <Input
+          <h1 style={{textAlign: "center", fontWeight:"bold"}}>Done: {countChecked()}</h1>
+          <Input 
             maxWidth="100%"
             overflow="hidden"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Add todo"
+            value={inputValue} 
+            onChange={(e) => setInputValue(e.target.value)} 
+            placeholder="Add todo" 
             onKeyDown={(e) => e.key === "Enter" && addTodo()}
           />
           <Button onClick={addTodo} mt={2}>Add</Button>
-        </div>
-      </div>
-    </div>
+      </Box>
+      </Card>
+    </Container>
+    </body>
+    </ChakraProvider>
   );
 }
 
@@ -113,37 +135,95 @@ function Counter() {
   }, [counter]);
 
     return (
-    <div className="counter-page">
-      <div className="container">
-        <div className="flex-column">
-          <div className="flex-row">
+      <Container
+        position="relative"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Flex
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          gap={10}
+        >
+          <Flex 
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            gap={10}  
+          >
             <Button onClick={() => setCounter(counter + 1)}>+</Button>
             <CountDisplay>{counter}</CountDisplay>
             <Button onClick={() => setCounter(counter - 1)}>-</Button>
-          </div>
-          <a href="/">Go back to home</a>
-        </div>
-      </div>
-    </div>
+          </Flex>
+          <Link to="/">Go back to home</Link>
+        </Flex>
+      </Container>
 
+  );
+}
+
+function Home() {
+  const navigate = useNavigate();
+
+  const [selectedComponent, setSelectedComponent] = useState("");
+
+  const navigateToComponent = () => {
+    if (selectedComponent) {
+      navigate(`/${selectedComponent.startsWith("/") ? selectedComponent.slice(1) : selectedComponent}`);
+    }
+  };
+
+  return (
+    useEffect(() => {
+      document.title = `Home`;
+    }, []),
+    <>
+      <main>
+            <Container
+              display="flex" 
+              flexDirection="column" 
+              justifyContent="center" 
+              alignItems="center" 
+              height="100vh"
+              width="100vw" 
+            >
+            <Flex justifyContent="center" alignItems="center" height="100vh" direction="column" gap={3}>
+            <img src="https://picsum.photos/200/300" style={{borderRadius: '20px'}}alt="random" />
+            <h1>Welcome to My Page!</h1>
+            <Select 
+              placeholder="Go to..."
+              value={selectedComponent}
+              background="white"
+              onChange={(e) => setSelectedComponent(e.target.value)}
+              >
+              <option value="/counter">Counter</option>
+              <option value="/todolist">ToDo List</option>
+            </Select>
+            <Button onClick={navigateToComponent}>Go!</Button>
+            </Flex>
+            </Container> 
+      </main>
+    </>
   );
 }
 
 function App() {
   return (
   <ChakraProvider>
+    <body style={{backgroundColor: "#eebc17", height: "100vh"}}>
     <Router>
         <Routes>
             <Route path="/counter" element={<Counter />} />
             <Route path="/todolist" element={<TodoList />} />
-            <Route path="/" element={
-              <>
-                <Link to="/counter">Go to Counter</Link>
-                <Link to="/todolist">Go to Todo List</Link>
-              </>
-            } />
+            <Route path="/" element={<Home />} />
         </Routes>
     </Router>
+    </body>
     </ChakraProvider>
   );
 }
