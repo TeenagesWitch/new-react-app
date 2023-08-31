f3a3e4aacac6b74a70a607fb0a5b063ad0c5b818
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-31 13:41:06 +0700

    fix counter

--- src/cart/cartPage.js
+++ src/cart/cartPage.js
@@ -4,7 +4,10 @@ import { Box, Heading, Table, Thead, Tbody, Tr, Th, Td, Image, Text } from '@cha
 import { fetchCartItems, updateItemQuantity, removeCartItem, selectCart } from './cartSlice'; // Importing the actions from cartSlice
 
 function CartPage() {
-  
+
+  // Step 1: State Maintenance
+  const [itemsToBeRemoved, setItemsToBeRemoved] = React.useState([]);
+
   const cart = useSelector(selectCart);
   const dispatch = useDispatch();
 
@@ -20,13 +23,35 @@ function CartPage() {
   }, [dispatch]);
   
   const handleQuantityChange = (id, newQuantity) => {
-    if (newQuantity === 0) {
-      dispatch(removeCartItem(id)); // Using the new action from cartSlice
-    } else {
-      // Update the quantity in the Redux store and backend
-      dispatch(updateItemQuantity({ id, quantity: newQuantity })); // Using the existing action from cartSlice
+    if (newQuantity < 0) {
+      return; // Don't allow negative quantities
     }
+    
+    if (newQuantity === 0) {
+      setItemsToBeRemoved(prevState => [...prevState, id]);
+    } else if (itemsToBeRemoved.includes(id)) {
+      // If the item's quantity is increased from 0, remove it from itemsToBeRemoved
+      setItemsToBeRemoved(prevState => prevState.filter(itemId => itemId !== id));
+    }
+    
+    dispatch(updateItemQuantity({ id, quantity: newQuantity }));
   };
+  
+  // Step 2: Event Listener
+  useEffect(() => {
+    const handleUnload = () => {
+      // Step 3: Check & Dispatch
+      itemsToBeRemoved.forEach(itemId => {
+        dispatch(removeCartItem(itemId));
+      });
+    };
+    
+    window.addEventListener("beforeunload", handleUnload);
+    
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, [itemsToBeRemoved, dispatch]);
 
   return (
     <Box p="4">
